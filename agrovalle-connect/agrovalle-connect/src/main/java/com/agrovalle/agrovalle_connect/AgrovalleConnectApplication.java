4f9cf62b474295a885cadff41a8d57cb2d8d7374
Kevin Andres Balanta Mezu <kandresbalanta@estudiante.uniajc.edu.co>
2026-09-18 22:01:30 -0500

    chore: configurar checkstyle y husky
    Integra Checkstyle en Maven y agrega el hook pre-commit para ejecutar las pruebas y la validacion de estilo.

--- agrovalle-connect/agrovalle-connect/src/main/java/com/agrovalle/agrovalle_connect/AgrovalleConnectApplication.java
+++ agrovalle-connect/agrovalle-connect/src/main/java/com/agrovalle/agrovalle_connect/AgrovalleConnectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgrovalleConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgrovalleConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgrovalleConnectApplication.class, args);
+    }
 
 }
